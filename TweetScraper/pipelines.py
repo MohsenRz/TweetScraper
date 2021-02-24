c8c2c8528d42aa5a0ee5c9123c3c868ac0d87ec8
radoo <mohsenrz.shahbakhsh@gmail.com>
2021-02-25 00:50:42 +0330

    fix missing imports and filter raw data

--- TweetScraper/pipelines.py
+++ TweetScraper/pipelines.py
@@ -1,6 +1,10 @@
 import os
 import logging
 import json
+import pymongo
+
+from itemadapter import ItemAdapter
+from datetime import datetime
 from scrapy.utils.project import get_project_settings
 
 from TweetScraper.items import Tweet, User
@@ -76,15 +80,37 @@ class MongoPipeline(object):
         self.client.close()
 
     def process_item(self, item, spider):
+
         if isinstance(item, Tweet):
             self.db[self.tweets].insert_one(
-                ItemAdapter(item['raw_data']).asdict())
+                ItemAdapter(self.filter_tweet(item['raw_data'])).asdict())
             logger.debug("Add tweet:%s" % item['id_'])
 
         elif isinstance(item, User):
             self.db[self.users].insert_one(
-                ItemAdapter(item['raw_data']).asdict())
+                ItemAdapter(self.filter_user(item['raw_data'])).asdict())
             logger.debug("Add user:%s" % item['id_'])
 
         else:
             logger.info("Item type is not recognized! type = %s" % type(item))
+
+    def filter_tweet(self, item):
+        new_item = {}
+        date = datetime.strptime(
+            item['created_at'], '%a %b %d %H:%M:%S %z %Y')
+        new_item['created_at'] = date.strftime("%Y-%m-%d %H:%M:%S")
+        new_item['id_str'] = item['id_str']
+        new_item['full_text'] = item['full_text']
+        new_item['favorite_count'] = item['favorite_count']
+        new_item['user_id_str'] = item['user_id_str']
+        # hashtags
+
+        return new_item
+
+    def filter_user(self, item):
+        new_item = {}
+        new_item['id_str'] = item['id_str']
+        new_item['name'] = item['name']
+        new_item['screen_name'] = item['screen_name']
+        new_item['description'] = item['description']
+        return new_item
